a2a8fd4dddb686d82b297c35349ae9f8b0976db4
zineb35 <116153861+zineb35@users.noreply.github.com>
2023-09-26 12:11:18 +0200

    chip8

--- chip8.go
+++ chip8.go
@@ -1,5 +1,12 @@
-import {
-	"github.com/faiface/pixel"
+package main
+
+type Chip8 struct {
+	Registers       [16]uint8
+	registerAddress [1]uint16
+	registerProgram [1]uint8
+	registerSound   [1]uint8
+	registerDelay   [1]uint8
+	stack           [16]uint8 // pile de 16 registres //
+	keyboard        [16]bool  // clavier de 16 touche //
 
 }
-type Chip
